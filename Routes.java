177685ba08ba03b52dcdb7185336f3df644b88ed
nferraro <ni.ferraro@gmail.com>
2019-04-02 00:14:21 +0200

    Adding retry when calling back Telegram

--- Routes.java
+++ Routes.java
@@ -8,23 +8,21 @@ public class Routes extends RouteBuilder {
 
         // Take all messages that are sent to our BOT
         from("telegram:bots/{{token}}")
+                // Just focus on the textual message
                 .convertBodyTo(String.class)
                 .choice()
-
                     .when(simple("${body.toLowerCase()} contains 'chuck'"))
                         // Let's retrieve data from the external service
-                        .to("direct:call")
-                        // Source is HTML encoded, so we tell the Telegram component we are sending HTML encoded data
-                        .setHeader(TelegramConstants.TELEGRAM_PARSE_MODE, constant(TelegramParseMode.HTML))
-                        // Send the quote back to the same chat (CamelTelegramChatId header is implicitly used)
-                        .to("telegram:bots/{{token}}")
+                        .to("direct:chuck")
+                        // We push data back to the telegram chat
+                        .to("direct:telegram")
                         .log("Returned: ${body}")
                     .otherwise()
                         // Just log all other messages
                         .log("Discarded: ${body}");
 
 
-        from("direct:call")
+        from("direct:chuck")
                 // Let's use a circuit breaker to avoid cascading failures
                 .hystrix().hystrixConfiguration().executionTimeoutInMilliseconds(1000).end()
                     // When a user writes the word 'chuck' on the chat execute the following steps
@@ -38,6 +36,13 @@ public class Routes extends RouteBuilder {
                 .end();
 
 
+        from("direct:telegram")
+                // Let's retry up to 5 times if something goes wrong
+                .errorHandler(defaultErrorHandler().maximumRedeliveries(5).redeliveryDelay(1000))
+                // Source is HTML encoded, so we tell the Telegram component we are sending HTML encoded data
+                .setHeader(TelegramConstants.TELEGRAM_PARSE_MODE, constant(TelegramParseMode.HTML))
+                // Send the quote back to the same chat (CamelTelegramChatId header is implicitly used)
+                .to("telegram:bots/{{token}}");
 
 
     }
